844f6d24882679545a5e92997db89c3a3d01b4ea
Alex <davidenko.olekciy.htcones@gmail.com>
2023-06-16 18:15:37 +0300

    add second screen

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Код для відкривання та закривання меню
 const hamburger = document.querySelector('.hamburger'),
     menu = document.querySelector('.menu'),
     closeElem = document.querySelector('.menu__close'),
@@ -14,3 +15,40 @@ closeElem.addEventListener('click', () => {
 closeAll.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+//-----------------------------------------------------------------
+// Код для зміни кольору бокової панелі
+window.addEventListener('scroll', function() {
+    var sidepanelLinks = document.querySelectorAll('.sidepanel__link');
+    var sidepanelDivider = document.querySelector('.sidepanel__divider');
+    var sidepanelText = document.querySelector('.sidepanel__text');
+  
+    var windowHeight = window.innerHeight;
+    var scrollHeight = document.documentElement.scrollHeight;
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  
+    var scrollThreshold = windowHeight * 0.6; // Значення прокрутки замість 75vh
+  
+    if (scrollTop >= scrollThreshold) {
+      sidepanelLinks.forEach(function(link) {
+        var paths = link.querySelectorAll('svg path');
+        paths.forEach(function(path) {
+          path.setAttribute('fill', 'black');
+        });
+      });
+      sidepanelDivider.style.backgroundColor = 'black';
+      sidepanelText.style.color = 'black';
+    } else {
+      sidepanelLinks.forEach(function(link) {
+        var paths = link.querySelectorAll('svg path');
+        paths.forEach(function(path) {
+          path.setAttribute('fill', '#ffffff');
+        });
+      });
+      sidepanelDivider.style.backgroundColor = '#ffffff';
+      sidepanelText.style.color = '#ffffff';
+    }
+  });
+//-----------------------------------------------------------------
+  
+  

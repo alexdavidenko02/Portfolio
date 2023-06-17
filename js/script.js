ecf68c8c698bcffdf5efc8b818cfe2fa7973ae7b
Alex <davidenko.olekciy.htcones@gmail.com>
2023-06-17 15:27:35 +0300

    add 3,4 & 5 screens

--- js/script.js
+++ js/script.js
@@ -50,5 +50,12 @@ window.addEventListener('scroll', function() {
     }
   });
 //-----------------------------------------------------------------
-  
-  
+// Переведення відсотків в skills
+const counters = document.querySelectorAll('.skills__ratings-counter'),
+      lines = document.querySelectorAll('.skills__ratings-line span');
+
+counters.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
+//-----------------------------------------------------------------
+
